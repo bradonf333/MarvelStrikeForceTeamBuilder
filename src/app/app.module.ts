18cc102827bb8d213a7d8a39cb7f89751faf0d6f
bfredrickson <bfredrickson@healthequity.com>
2019-07-22 11:50:38 -0600

    welcome component and nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,20 @@ import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { BrowserModule } from '@angular/platform-browser';
 /* Bootstrap */
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { IconsModule, MDBBootstrapModule } from 'angular-bootstrap-md';
 import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavBarComponent, WelcomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MDBBootstrapModule.forRoot(),
+    IconsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule.enablePersistence()
   ],
