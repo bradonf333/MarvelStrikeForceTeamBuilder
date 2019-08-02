7b3f698fd7796aad666301912081306a3fef8076
bfredrickson <bfredrickson@healthequity.com>
2019-08-02 15:45:02 -0600

    Fixed hosting

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCwlko8SAAM-r5TZXlW9AFccKS3edTFHAE',
+    authDomain: 'msf-team-builder-3f2b8.firebaseapp.com',
+    databaseURL: 'https://msf-team-builder-3f2b8.firebaseio.com',
+    projectId: 'msf-team-builder-3f2b8',
+    storageBucket: 'msf-team-builder-3f2b8.appspot.com',
+    messagingSenderId: '425252600046',
+    appId: '1:425252600046:web:e61d7db092d80a4f'
+  }
+};
